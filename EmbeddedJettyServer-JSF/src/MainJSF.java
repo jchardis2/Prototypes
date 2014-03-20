84a3ffd784af3146362b809ebb51a8bc6e8fa213
Jimmy Hardison <jchardis2@gmail.com>
2014-03-20 17:41:29 -0400

    Test

--- EmbeddedJettyServer-JSF/src/MainJSF.java
+++ EmbeddedJettyServer-JSF/src/MainJSF.java
@@ -1,7 +1,6 @@
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.servlet.ServletHandler;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class MainJSF {
